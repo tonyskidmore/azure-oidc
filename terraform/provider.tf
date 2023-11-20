992ea27d6642fe8c8d919861f1d3875bb7daa1c6
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-11-20 20:29:17 +0000

    adding example terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+
+# Azure Provider: Authenticating using a Service Principal with Open ID Connect
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_oidc
+# azurerm
+# https://developer.hashicorp.com/terraform/language/settings/backends/azurerm
+#   backend "azurerm" {
+#     resource_group_name  = "rg-azure-github-oidc-test"
+#     storage_account_name = "abcd1234"
+#     container_name       = "tfstate"
+#     key                  = "azure-oidc.tfstate"
+#     use_oidc             = true
+#   }
+
+  required_version = ">= 1.3.0, < 2"
+
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.7.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  use_oidc                   = true
+  features {}
+}
