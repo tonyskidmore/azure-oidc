ea292d3d3b296f738f4a2ebd487eaa8fdde5c3d0
Tony Skidmore <16082810+tonyskidmore@users.noreply.github.com>
2023-11-20 20:38:15 +0000

    tidying up

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,13 +4,17 @@ terraform {
   # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/service_principal_oidc
   # azurerm
   # https://developer.hashicorp.com/terraform/language/settings/backends/azurerm
-  #   backend "azurerm" {
-  #     resource_group_name  = "rg-azure-github-oidc-test"
-  #     storage_account_name = "abcd1234"
-  #     container_name       = "tfstate"
-  #     key                  = "azure-oidc.tfstate"
-  #     use_oidc             = true
-  #   }
+
+  # not using backend "azurerm" because it requires a storage account
+  # but if using OIDC define it for the backend too and comment out the provider block below
+  # and configure the settings for the Terraform backend
+  # backend "azurerm" {
+  #   resource_group_name  = "rg-azure-github-oidc-test"
+  #   storage_account_name = "abcd1234"
+  #   container_name       = "tfstate"
+  #   key                  = "azure-oidc.tfstate"
+  #   use_oidc             = true
+  # }
 
   required_version = ">= 1.3.0, < 2"
 
